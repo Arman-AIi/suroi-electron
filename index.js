ebb00567e778d2213ff366a1d4199588a8042a1c
Arman <armanali.aban@gmail.com>
2024-10-16 22:24:46 -0500

    -typescript compiler -added package-lock.json -modified package.json

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-const { app, BrowserWindow } = require('electron/main')
-
-const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 1920,
-    height: 1080
-  })
-
-  win.loadFile('client/index.html')
-}
-
-app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+var _a = require('electron/main'), app = _a.app, BrowserWindow = _a.BrowserWindow;
+var createWindow = function () {
+    var win = new BrowserWindow({
+        width: 1920,
+        height: 1080
+    });
+    win.loadURL('http://localhost:3000');
+};
+app.whenReady().then(function () {
+    createWindow();
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
     }
-  })
-})
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+});
