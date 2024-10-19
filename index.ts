0a74531d62a29f1394778efa00b78e35be9af794
Arman <armanali.aban@gmail.com>
2024-10-18 20:30:48 -0500

    - electron app window now autofits to the user's screen resolution - removed ugly scrollbar in the loading screen - removed unused svg

--- index.ts
+++ index.ts
@@ -1,33 +1,36 @@
-const { app, BrowserWindow } = require('electron/main')
+const { app, BrowserWindow, screen: electronScreen } = require('electron/main');
 
 const createWindow = () => {
+  const { width, height } = electronScreen.getPrimaryDisplay().workAreaSize;
+
   const win = new BrowserWindow({
-    width: 1920,
-    height: 1080,
+    width: width,
+    height: height,
     frame: false,
-    backgroundColor:'hsl(28, 100%, 50%)'
-  })
-  win.loadFile('client/loading.html')
+    backgroundColor: 'hsl(28, 100%, 50%)'
+  });
+
+  win.loadFile('client/loading.html');
+
   setTimeout(() => {
     win.reload();
-    win.loadURL('http://localhost:3000')
+    win.loadURL('http://localhost:3000');
   }, 20000);
-}
+};
 
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-      
+      createWindow();
     }
-  })
-})
+  });
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
